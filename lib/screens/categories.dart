4e6a790010789b16f0f5c4a724cbd6b796178356
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2023-12-11 21:59:25 +0100

    navigating to category meals screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    // Navigator.push(context, route);
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const MealsScreen(
+          title: 'Some Title',
+          meals: [],
+          categoryId: '',
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +36,9 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList();
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(category: category, onSelectCategory: () { 
+              _selectCategory(context);
+             },)
         ],
       ),
     );
